9c8be5d931596c9e54fe3fc10198eefe7159f514
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2025-02-07 07:48:48 +0200

    Change IDbContext commandTimeout from null to 0.

--- Interfaces/IDbContext.cs
+++ Interfaces/IDbContext.cs
@@ -10,16 +10,16 @@ public interface IDbContext
 
     #region Generic data operations
 
-    Task<int> Execute(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null);
-    Task<T?> ExecuteScalar<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null);
+    Task<int> Execute(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0);
+    Task<T?> ExecuteScalar<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0);
     Task<T?> QueryFirst<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout=null);
-    Task<IEnumerable<T>> Query<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null);
-    Task<IEnumerable<T>> QueryProcedure<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null);
-    Task<T?> QueryProcedureScalar<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null) where T : struct;
+    Task<IEnumerable<T>> Query<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0);
+    Task<IEnumerable<T>> QueryProcedure<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0);
+    Task<T?> QueryProcedureScalar<T>(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0) where T : struct;
 
-    Task<int> ExecuteProcedure(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = null);
+    Task<int> ExecuteProcedure(string sql, object? parameters = null, IDbTransaction? transaction = null, int? commandTimeout = 0);
 
-    Task<IEnumerable<T>> QueryTable<T>(string tableName,IDbTransaction? transaction = null, int? commandTimeout = null);
+    Task<IEnumerable<T>> QueryTable<T>(string tableName,IDbTransaction? transaction = null, int? commandTimeout = 0);
 
     #endregion
 
@@ -35,13 +35,13 @@ public interface IDbContext
     #region DataTable and Tables
     string PrefixForTempTables { get; init; }
     bool TableExists(string tableName, string? schema = null, IDbConnection? connection = null);
-    Task<string> InitializeTempTable<T>(IEnumerable<T> items, string createTableSqlBody, int? bulkTimeout = null);
+    Task<string> InitializeTempTable<T>(IEnumerable<T> items, string createTableSqlBody, int? bulkTimeout = 0);
 
     DataTable InitializeDatatable<TEntity>(IEnumerable<TEntity> items);
     string GetNewTempTableName(IDbConnection connection, string tableNamePrefix);
     string GetNewTempTableName(string tableNamePrefix);
     Task<(int Updated, int Added)> UpdateOldAndAddNew<T>(List<T> items, string mainTable, string createTableSqlBody, string addAndUpdateSql,
-        Func<List<T>, string, Task>? PostAction = null, int? commandTimeout = null);
+        Func<List<T>, string, Task>? PostAction = null, int? commandTimeout = 0);
 
     #endregion
 }
