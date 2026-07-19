4c0ac49696cad6438282aead97a10e9cd52aa002
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2026-07-19 12:02:13 +0300

    change default namespace to DapperContextAlt

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DapperContext.Interfaces;
+﻿namespace DapperContext.Interfaces;
 
 //public interface IRepository<T_Db> : IRepository<T_Db, T_Db> { }
 
